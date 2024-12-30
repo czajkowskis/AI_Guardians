5014537445a97f8bec33ca1ff7d76e50c8e0870a
czajkowskis <szymonczajkowski022@gmail.com>
2024-12-30 16:13:40 +0100

    Added animation when changing the view

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,27 @@
 </script> 
 
 <template>
-  <div id="app">
-    <router-view></router-view>
+  <div id="app">  
+    <router-view v-slot="{Component}">
+      <transition name="slide">
+        <component :is="Component" />
+      </transition>
+    </router-view>
   </div>
 </template>
 
 <style scoped>
+
+  .slide-enter-from{
+    transform: translateX(0px);
+  }
+
+  .slide-enter-active,
+  .slide-leave-active {
+    transition: all 1s ease-in-out;
+  }
+
+  .slide-leave-to{
+    transform: translateX(100%)
+  }
 </style>
